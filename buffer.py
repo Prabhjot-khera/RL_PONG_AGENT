2acb38f3915431339ae262c018fc809bee7b6f5d
pktubs <p.khera2004@gmail.com>
2025-07-14 21:22:28 -0400

    sampling and storing in buffer

--- buffer.py
+++ buffer.py
@@ -13,3 +13,34 @@ class ReplayBuffer():
 
         self.device = device
 
+    def can_sample(self, batch_size):
+        return self.mem_ctr > batch_size*5
+    
+    def store_transition(self, state, action, reward, next_state, done):
+        index = self.mem_ctr % self.mem_size
+
+        self.state_memory[index] = state
+        self.next_state_memory[index] = next_state
+        self.action_memory[index] = torch.tensor(action).detach().cpu()
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+
+        self.mem_ctr += 1
+
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_ctr, self.mem_size)
+        batch = np.random.choice(max_mem, batch_size)
+
+        states = self.state_memory[batch]
+        next_states = self.next_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        dones = self.terminal_memory[batch]
+
+        states = torch.tensor(states, dtype=torch.float32).to(self.device)
+        next_states = torch.tensor(next_states, dtype=torch.float32).to(self.device)
+        actions = torch.tensor(actions, dtype=torch.float32).to(self.device)
+        rewards = torch.tensor(rewards, dtype=torch.float32).to(self.device)
+        dones = torch.tensor(dones, dtype=torch.bool).to(self.device)
+        
+        return states, actions, rewards, next_states, dones
